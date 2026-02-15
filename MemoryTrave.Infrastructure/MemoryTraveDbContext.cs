7a924ffdf5b23eec2382d503215e7a581b0a58f5
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-16 00:29:23 +0300

    add: DbContext with DbSet #3

---
+++ MemoryTrave.Infrastructure/MemoryTraveDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MemoryTrave.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoryTrave.Infrastructure;
+
+public class MemoryTraveDbContext(DbContextOptions<MemoryTraveDbContext> options) : DbContext(options)
+{
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<ArticleAccess>  ArticleAccesses { get; set; }
+    public DbSet<FriendRequest> FriendRequests { get; set; }
+    public DbSet<Friendship>  Friendships { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<User> Users { get; set; }
+}
