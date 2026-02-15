3847739965d690d55f1a8e8e91f9237a419a32db
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-16 01:05:30 +0300

    add: location configuration #3

---
+++ MemoryTrave.Infrastructure/Configurations/LocationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using MemoryTrave.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MemoryTrave.Infrastructure.Configurations;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.HasKey(l => l.Id);
+        builder.Property(l => l.Name).ValueGeneratedNever();
+        
+        builder.Property(l => l.Name).IsRequired();
+        builder.Property(l=>l.Latitude).IsRequired();
+        builder.Property(l=>l.Longitude).IsRequired();
+        builder.Property(l=>l.Geohash)
+            .IsRequired()
+            .HasMaxLength(12);
+        
+        builder.HasMany(l => l.Articles)
+            .WithOne(a => a.Location)
+            .HasForeignKey(a => a.LocationId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
