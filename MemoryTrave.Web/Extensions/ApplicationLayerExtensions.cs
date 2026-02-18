d0197c0571d0c5053d1ea58db050d6aba02ab125
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-18 22:41:17 +0300

    add: application layer extensions #14

---
+++ MemoryTrave.Web/Extensions/ApplicationLayerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using MemoryTrave.Application.Validators.Requests.User;
+
+namespace MemoryTrave.Web.Extensions;
+
+public static class ApplicationLayerExtensions
+{
+    public static void AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<RegistrationRequestValidator>();
+    }
+}
