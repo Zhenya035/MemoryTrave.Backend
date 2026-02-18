66e0b58c4bd09595cdaa4b46f6c59ea24246ca4b
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-19 00:09:18 +0300

    add: jwt extensions #6

---
+++ MemoryTrave.Web/Extensions/JwtExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using MemoryTrave.Application.Services;
+using MemoryTrave.Infrastructure.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MemoryTrave.Web.Extensions;
+
+public static class JwtExtensions
+{
+    public static void AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        services.AddScoped<IJwtService, JwtService>();
+
+        var settings = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+        if(settings == null)
+            throw new InvalidOperationException("JwtSettings not configured");
+        
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = settings.Issuer,
+                    ValidAudience = settings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.SecretKey))
+                };
+                o.MapInboundClaims = false;
+            });
+    }
+}
