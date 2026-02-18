43255dc60cb927c0d2e0df593d5c02e6f78ca2df
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-18 22:40:42 +0300

    add: user controller #14

---
+++ MemoryTrave.Web/Contollers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MemoryTrave.Application.Dto.Requests.User;
+using MemoryTrave.Application.Dto.Responses.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoryTrave.Web.Contollers;
+
+[ApiController]
+[Route("users")]
+public class UserController(IValidator<RegistrationDto> _validator) : ControllerBase
+{
+    [HttpPost("registration")]
+    public async Task<ActionResult<AuthorizationResponseDto>> Registration([FromBody] RegistrationDto user)
+    {
+        var result = await _validator.ValidateAsync(user);
+        
+        if(!result.IsValid)
+            BadRequest(result.Errors);
+        
+        return Ok(result);
+    }
+}
