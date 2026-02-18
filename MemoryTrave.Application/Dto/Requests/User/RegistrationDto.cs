3fcb88a826a6d61e4555e849db18e064fa30fb03
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-18 22:39:58 +0300

    add: authorization dtos #14

---
+++ MemoryTrave.Application/Dto/Requests/User/RegistrationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MemoryTrave.Application.Dto.Requests.User;
+
+public class RegistrationDto
+{
+    public string Email { get; set; } =  string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string PublicKey { get; set; } = string.Empty;
+    public string EncryptedPrivateKey { get; set; } = string.Empty;
+}
