c5cb2fc93fae759f3cf74d81b3d4b7a29549ad64
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-19 00:06:39 +0300

    add: jwt settings #6

---
+++ MemoryTrave.Infrastructure/Jwt/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MemoryTrave.Infrastructure.Jwt;
+
+public class JwtSettings
+{
+    public string SecretKey { get; set; } = string.Empty;
+    public TimeSpan Expires { get; set; }
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+}
