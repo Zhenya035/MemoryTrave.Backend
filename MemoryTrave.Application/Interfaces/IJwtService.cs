df7199f927d41fbf877bef8268ac5c0b9bce61e6
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-19 00:08:54 +0300

    add: jwt service #6

---
+++ MemoryTrave.Application/Interfaces/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Application.Services;
+
+public interface IJwtService
+{
+    string GenerateJwt(User user);
+}
