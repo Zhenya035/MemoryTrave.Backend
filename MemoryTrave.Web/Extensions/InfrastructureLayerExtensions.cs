c335a0c75538a7f99aa75df2f3650ad47d2cf3ae
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-20 00:38:38 +0300

    add: infrastructure extensions #14

---
+++ MemoryTrave.Web/Extensions/InfrastructureLayerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MemoryTrave.Domain.Interfaces;
+using MemoryTrave.Infrastructure.Repositories;
+
+namespace MemoryTrave.Web.Extensions;
+
+public static class InfrastructureLayerExtensions
+{
+    public static void AddInfrastructureLayer(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+}
