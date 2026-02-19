b7a4f6921460a535605de22e6b8fa659113a2b61
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-20 00:38:06 +0300

    up(extensions): add services #14

--- MemoryTrave.Web/Extensions/ApplicationLayerExtensions.cs
+++ MemoryTrave.Web/Extensions/ApplicationLayerExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using FluentValidation;
+using MemoryTrave.Application.Interfaces.User;
+using MemoryTrave.Application.Services;
+using MemoryTrave.Application.Services.User;
 using MemoryTrave.Application.Validators.Requests.User;
 
 namespace MemoryTrave.Web.Extensions;
@@ -8,5 +11,9 @@ public static class ApplicationLayerExtensions
     public static void AddApplicationLayer(this IServiceCollection services)
     {
         services.AddValidatorsFromAssemblyContaining<RegistrationRequestValidator>();
+
+        services.AddScoped<IAuthorizationUseCase, AuthorizationUseCase>();
+        services.AddScoped<IRegistrationUseCase, RegistrationUseCase>();
+        services.AddScoped<IUserService, UserService>();
     }
 }
