20687ac13a3c8b93aa3850a0a92cc0949bf1cf9b
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-19 03:39:07 +0300

    add: authorization use-case #14

---
+++ MemoryTrave.Application/Services/User/AuthorizationUseCase.cs
@@ -0,0 +1,34 @@
+﻿using MemoryTrave.Application.Dto.Requests.User;
+using MemoryTrave.Application.Dto.Responses.User;
+using MemoryTrave.Application.Interfaces;
+using MemoryTrave.Application.Interfaces.User;
+using MemoryTrave.Domain.Exceptions;
+using MemoryTrave.Domain.Interfaces;
+
+namespace MemoryTrave.Application.Services.User;
+
+public class AuthorizationUseCase(IUserRepository userRepository, IJwtService jwtService) : IAuthorizationUseCase
+{
+    public async Task<AuthorizationResponseDto> Authorization(AuthorizationDto authUser)
+    {
+        if (await userRepository.UserExists(authUser.Email))
+            throw new AlreadyAddedException("This email");
+
+        var user = await userRepository.GetByEmail(authUser.Email);
+        
+        if (user == null)
+            throw new NotFoundException("User");
+        
+        if(!BCrypt.Net.BCrypt.Verify(authUser.Password, user.PasswordHash))
+            throw new InvalidInputDataException("Password is incorrect");
+        
+        var token = jwtService.GenerateJwt(user);
+
+        var response = new AuthorizationResponseDto()
+        {
+            JwtToken = token,
+        };
+        
+        return response;
+    }
+}
