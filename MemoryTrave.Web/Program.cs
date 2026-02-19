3e88fc07abeec82223765f1158daa5bbe5801d92
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-20 00:41:28 +0300

    add: swagger #14

--- MemoryTrave.Web/Program.cs
+++ MemoryTrave.Web/Program.cs
@@ -1,4 +1,6 @@
+using MemoryTrave.Application.Interfaces;
 using MemoryTrave.Infrastructure;
+using MemoryTrave.Web;
 using MemoryTrave.Web.Extensions;
 using MemoryTrave.Web.Middlewares;
 using Microsoft.EntityFrameworkCore;
@@ -11,13 +13,23 @@ builder.Services.AddDbContext<MemoryTraveDbContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString(nameof(MemoryTraveDbContext)));
 });
 
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddInfrastructureLayer();
 builder.Services.AddApplicationLayer();
+builder.Services.AddScoped<ICurrentUserProvider, CurrentUserProvider>();
 
 builder.Services.AddJwt(configuration);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
