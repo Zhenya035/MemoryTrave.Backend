49b50891867b4fb49ca11483ae93f3fde82fc053
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-19 03:39:59 +0300

    add: registration use-case #14

---
+++ MemoryTrave.Application/Interfaces/User/IRegistrationUseCase.cs
@@ -0,0 +1,9 @@
+﻿using MemoryTrave.Application.Dto.Requests.User;
+using MemoryTrave.Application.Dto.Responses.User;
+
+namespace MemoryTrave.Application.Services.User;
+
+public interface IRegistrationUseCase
+{
+    public Task<AuthorizationResponseDto> Registration(RegistrationDto regUser);
+}
---
+++ MemoryTrave.Application/Services/User/RegistrationUseCase.cs
@@ -0,0 +1,33 @@
+﻿using MemoryTrave.Application.Dto.Requests.User;
+using MemoryTrave.Application.Dto.Responses.User;
+using MemoryTrave.Application.Interfaces;
+using MemoryTrave.Application.Mapping;
+using MemoryTrave.Domain.Exceptions;
+using MemoryTrave.Domain.Interfaces;
+
+namespace MemoryTrave.Application.Services.User;
+
+public class RegistrationUseCase(
+    IUserRepository userRepository,
+    IJwtService jwtService) : IRegistrationUseCase
+{
+    public async Task<AuthorizationResponseDto> Registration(RegistrationDto regUser)
+    {
+        if (await userRepository.UserExists(regUser.Email))
+            throw new AlreadyAddedException("This email");
+        
+        var user = UserMapping.MapFromRegistrationDto(regUser);
+        user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(regUser.Password);
+
+        user = await userRepository.Registration(user);
+        
+        var token = jwtService.GenerateJwt(user);
+
+        var response = new AuthorizationResponseDto()
+        {
+            JwtToken = token,
+        };
+        
+        return response;
+    }
+}
