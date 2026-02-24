a60d5dac69f654adf8f2f2c62200632efbaea6d7
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-24 18:46:06 +0300

    add: friendship repository interface #14

---
+++ MemoryTrave.Domain/Interfaces/IFriendshipRepository.cs
@@ -0,0 +1,8 @@
+﻿using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Domain.Interfaces;
+
+public interface IFriendshipRepository
+{
+    public Task<List<Guid>> GetAllFriends(Guid userId);
+}
