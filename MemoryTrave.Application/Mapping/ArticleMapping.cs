51dc5207b5b2b2daf37b5e3ce81107ef16a193e8
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-24 18:42:27 +0300

    add: article mapping #14

---
+++ MemoryTrave.Application/Mapping/ArticleMapping.cs
@@ -0,0 +1,19 @@
+﻿using MemoryTrave.Application.Dto.Responses.Article;
+using MemoryTrave.Domain.Enums;
+using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Application.Mapping;
+
+public static class ArticleMapping
+{
+    public static GetArticleForProfileDto MapToGetArticleForProfileDto(Article article) =>
+        new()
+        {
+            Id = article.Id,
+            LocationName = article.Location.Name,
+            LastChange = article.LastChange,
+            IsPrivate = article.Visibility == VisibilityEnum.Private,
+            EncryptedPreviewData = article.Visibility == VisibilityEnum.Private ?  article.EncryptedPreviewData : null,
+            Description = article.Visibility == VisibilityEnum.Public ? article.Description : null
+        };
+}
