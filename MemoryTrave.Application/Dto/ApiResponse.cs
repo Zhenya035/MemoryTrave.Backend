b3d134536da1e1304745194b98fd96fc26cf6d20
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-25 21:32:41 +0300

    add: base controller

---
+++ MemoryTrave.Application/Dto/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace MemoryTrave.Application.Dto;
+
+public class ApiResponse
+{
+    public bool Success { get; set; }
+    public string? Message { get; set; }
+    public object? Errors { get; set; }
+}
+
+public class ApiResponse<T> : ApiResponse
+{
+    public T? Data { get; set; }
+}
---
+++ MemoryTrave.Web/Controllers/BaseController.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using FluentValidation.Results;
+using MemoryTrave.Application.Dto;
+using MemoryTrave.Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoryTrave.Web.Controllers;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    protected IActionResult Ok<T>(T data, string? message = null) =>
+        base.Ok(new ApiResponse<T>
+        {
+            Success = true,
+            Data = data,
+            Message = message
+        });
+
+    protected IActionResult Ok() =>
+        base.Ok(new ApiResponse { Success = true });
+
+    protected IActionResult Created<T>(T data) =>
+        StatusCode(201, new ApiResponse<T>
+        {
+            Success =  true,
+            Data = data
+        });
+
+
+    protected IActionResult BadRequest(ValidationResult result)
+    {
+        var formattedErrors = result.Errors.Select(e => new
+        {
+            field = e.PropertyName,
+            error = e.ErrorMessage
+        }).ToList();
+        
+        return base.BadRequest(new ApiResponse
+        {
+            Success = false,
+            Message = "Validation failed",
+            Errors = formattedErrors
+        });
+    }
+
+    protected Guid GetCurrentUserId()
+    {
+        var claim = User.FindFirst("id") ?? User.FindFirst(ClaimTypes.NameIdentifier);
+        if (claim == null || !Guid.TryParse(claim.Value, out var userId))
+            throw new UnAuthorizedException("token");
+        
+        return userId;
+    }
+}
