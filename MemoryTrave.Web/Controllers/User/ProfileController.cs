6b084f51f034e26c77a7ea98cdab956b40685e70
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-26 01:39:23 +0300

    up: change folder name #4

---
+++ MemoryTrave.Web/Controllers/User/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using MemoryTrave.Application.Interfaces.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoryTrave.Web.Controllers.User;
+
+[ApiController]
+[Route("users/profile")]
+[Authorize]
+public class ProfileController(IUserService service) : BaseController
+{
+    [HttpGet("my")]
+    public async Task<IActionResult> GetMyProfile()
+    {
+        var userId = GetCurrentUserId();
+        var result = await service.GetProfile(userId);
+        return Ok(result);
+    }
+
+    [HttpGet("blocks")]
+    public async Task<IActionResult> GetBlockUsers()
+    {
+        var userId = GetCurrentUserId();
+        
+        var result = await service.GetBlockUsers(userId);
+        return Ok(result);
+    }
+}
