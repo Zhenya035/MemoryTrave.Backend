e1f618ff760cac1b06201fc6b2f39183aa7f2968
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-27 01:54:23 +0300

    add: airicle access repository #4

---
+++ MemoryTrave.Domain/Interfaces/IArticleAccessRepository.cs
@@ -0,0 +1,14 @@
+﻿using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Domain.Interfaces;
+
+public interface IArticleAccessRepository
+{
+    public Task AddList(List<ArticleAccess> articleAccesses);
+    public Task Add(ArticleAccess articleAccess);
+    
+    public Task DeleteList(List<Guid> ids);
+    public Task Delete(Guid id);
+    
+    public Task<bool> Exists(Guid articleId, Guid userId);
+}
