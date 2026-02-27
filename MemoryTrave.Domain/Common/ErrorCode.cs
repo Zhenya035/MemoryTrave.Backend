891f18ccd304f10f894fe8cf21de473c0519e25e
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-02-28 00:24:28 +0300

    add: result pattern #4

---
+++ MemoryTrave.Domain/Common/ErrorCode.cs
@@ -0,0 +1,10 @@
+﻿namespace MemoryTrave.Domain.Common;
+
+public enum ErrorCode
+{
+    InvalidInput = 400,
+    UserBanned = 401,
+    Unauthorized = 403,
+    NotFound = 404,
+    AlreadyExists = 409
+}
