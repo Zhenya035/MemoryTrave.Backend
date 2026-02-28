f473e2eded1a8ab3ad3186d6b0206d755e9b443e
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:07:32 +0300

    add: article service with interface #4

---
+++ MemoryTrave.Application/Interfaces/Article/IArticleService.cs
@@ -0,0 +1,17 @@
+﻿using MemoryTrave.Application.Dto.Requests.Article;
+using MemoryTrave.Application.Dto.Responses.Article.GetArticle;
+using MemoryTrave.Domain.Common;
+
+namespace MemoryTrave.Application.Interfaces.Article;
+
+public interface IArticleService
+{
+    public Task<Result<GetArticleBaseDto>> GetByIdWithIncludes(Guid articleId, Guid userId);
+    
+    public Task<Result> AddPrivate(AddPrivateArticleDto dto, Guid authorId);
+    public Task<Result> AddPublic(AddPublicArticleDto dto, Guid authorId);
+    
+    public Task<Result> Update(UpdateArticleDto dto, Guid articleId);
+   
+    public Task<Result> Delete(Guid articleId);
+}
---
+++ MemoryTrave.Application/Services/Article/ArticleService.cs
@@ -0,0 +1,137 @@
+﻿using AutoMapper;
+using MemoryTrave.Application.Dto.Requests.Article;
+using MemoryTrave.Application.Dto.Responses.Article.GetArticle;
+using MemoryTrave.Application.Interfaces;
+using MemoryTrave.Application.Interfaces.Article;
+using MemoryTrave.Domain.Common;
+using MemoryTrave.Domain.Enums;
+using MemoryTrave.Domain.Interfaces;
+using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Application.Services.Article;
+
+public class ArticleService(
+    IArticleRepository repository, 
+    IArticleAccessRepository accessRepository,
+    IMapper mapper, 
+    IValidationService validationService) : IArticleService
+{
+    public async Task<Result<GetArticleBaseDto>> GetByIdWithIncludes(Guid articleId, Guid userId)
+    {
+        if (articleId == Guid.Empty)
+            return Result<GetArticleBaseDto>.Failure("Invalid article ID", ErrorCode.InvalidInput);
+        
+        if (userId == Guid.Empty)
+            return Result<GetArticleBaseDto>.Failure("Invalid user ID", ErrorCode.InvalidInput);
+        
+        var isExist = await repository.IsExists(articleId);
+        if (!isExist)
+            return Result<GetArticleBaseDto>.Failure("Article not found", ErrorCode.NotFound);
+
+        var article = await repository.GetByIdWithIncludes(articleId);
+        if (article.Visibility == VisibilityEnum.Public)
+        {
+            var publicArticleDto = mapper.Map<GetPublicArticleDto>(article);
+            return Result<GetArticleBaseDto>.Success(publicArticleDto);
+        }
+
+        var access = article.EncryptedKeys.FirstOrDefault(k => k.UserId == userId);
+        if (access == null)
+            return Result<GetArticleBaseDto>.Failure("Access denied", ErrorCode.Forbidden);
+        
+        var privateArticleDto = mapper.Map<GetPrivateArticleDto>(article);
+        privateArticleDto.EncryptedKey = access.EncryptedKey;
+        
+        return Result<GetArticleBaseDto>.Success(privateArticleDto);
+    }
+
+    public async Task<Result> AddPrivate(AddPrivateArticleDto dto, Guid authorId)
+    {
+        var validResult = await validationService.Validate(dto);
+        if (!validResult.IsSuccess)
+            return Result.Failure(validResult.Error, validResult.ErrorCode);
+        
+        var article = mapper.Map<Domain.Models.Article>(dto);
+        article.Id = Guid.NewGuid();
+        article.Visibility = VisibilityEnum.Private;
+        article.CreatedAt = DateTime.UtcNow;
+        article.LastChange = DateTime.UtcNow;
+        article.AuthorId = authorId;
+        
+        var articleId = await repository.Add(article);
+        
+        var encryptedKeys = dto.EncryptedKeys.Select(mapper.Map<ArticleAccess>).ToList();
+        foreach (var articleAccess in encryptedKeys)
+            articleAccess.ArticleId = articleId;
+        await accessRepository.AddList(encryptedKeys);
+        
+        return Result.Success();
+    }
+
+    public async Task<Result> AddPublic(AddPublicArticleDto dto, Guid authorId)
+    {
+        var validResult = await validationService.Validate(dto);
+        if (!validResult.IsSuccess)
+            return Result.Failure(validResult.Error, validResult.ErrorCode);
+        
+        var article = mapper.Map<Domain.Models.Article>(dto);
+        article.Id = Guid.NewGuid();
+        article.Visibility = VisibilityEnum.Public;
+        article.CreatedAt = DateTime.UtcNow;
+        article.LastChange = DateTime.UtcNow;
+        article.AuthorId = authorId;
+        
+        await repository.Add(article);
+        return Result.Success();
+    }
+
+    public async Task<Result> Update(UpdateArticleDto dto, Guid articleId)
+    {
+        var validResult = await validationService.Validate(dto);
+        if(!validResult.IsSuccess)
+            return Result.Failure(validResult.Error, validResult.ErrorCode);
+
+        var article = await repository.GetByIdWithIncludes(articleId);
+        if (article == null)
+            return Result.Failure("Article not found", ErrorCode.NotFound);
+        
+        if (dto.Visibility == VisibilityEnum.Private && dto.EncryptedKeys.All(k => k.UserId != article.AuthorId))
+            return Result.Failure("Invalid keys", ErrorCode.InvalidInput);
+        
+        var upArticle = mapper.Map<Domain.Models.Article>(dto);
+        upArticle.LastChange = DateTime.UtcNow;
+        
+        if (upArticle.Visibility == VisibilityEnum.Private)
+        {
+            upArticle.Description = null;
+            upArticle.PhotosUrls = null;
+
+            var encryptedKeys = dto.EncryptedKeys.Select(mapper.Map<ArticleAccess>).ToList();
+            await accessRepository.Sync(articleId, encryptedKeys);
+        }
+        else
+        {
+            upArticle.EncryptedPreviewData = null;
+            upArticle.EncryptedData = null;
+
+            if (article.Visibility == VisibilityEnum.Private)
+            {
+                await accessRepository.DeleteForArticle(articleId);
+            }
+        }
+        
+        await repository.Update(upArticle, articleId);
+        
+        return Result.Success();
+    }
+
+    public async Task<Result> Delete(Guid articleId)
+    {
+        var isExist = await repository.IsExists(articleId);
+        if (!isExist)
+            return Result.Failure("Article not found", ErrorCode.NotFound);
+        
+        await repository.Delete(articleId);
+        return Result.Success();
+    }
+}
