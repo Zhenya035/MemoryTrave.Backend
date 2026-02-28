d1c2c57f2d2fc4710cd05df3a5766dc3e15016e6
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:08:41 +0300

    add: articleAccess mapping #4

---
+++ MemoryTrave.Application/Mapping/ArticleAccessMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MemoryTrave.Application.Dto.Requests.Article.Access;
+using MemoryTrave.Application.Dto.Responses.Article.Access;
+using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Application.Mapping;
+
+public class ArticleAccessMappingProfile : Profile
+{
+    public ArticleAccessMappingProfile()
+    {
+        CreateMap<ArticleAccess, GetArticleAccessDto>();
+        
+        CreateMap<AddAccessDto, ArticleAccess>()
+            .ForMember(mod => mod.Id,
+                opt => opt.Ignore())
+            .ForMember(mod => mod.ArticleId,
+                opt => opt.Ignore());
+    }
+}
