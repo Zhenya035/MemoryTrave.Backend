fb5dff55d9f3340e5018bdf2085f2f2da1d1c546
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:09:21 +0300

    up(articleController): add realization #4

--- MemoryTrave.Web/Controllers/Article/ArticleController.cs
+++ MemoryTrave.Web/Controllers/Article/ArticleController.cs
@@ -1,45 +1,53 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MemoryTrave.Application.Dto.Requests.Article;
+using MemoryTrave.Application.Dto.Responses.Article;
+using MemoryTrave.Application.Interfaces.Article;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MemoryTrave.Web.Controllers.Article;
 
 [Route("articles")]
 [Authorize]
-public class ArticleController : BaseController
+public class ArticleController(IArticleService service, IWebHostEnvironment env) : BaseController(env)
 {
     [HttpGet("{articleId}")]
     public async Task<IActionResult> Get(Guid articleId)
     {
-        return Success();
+        var userId = GetCurrentUserId();
+        
+        var result = await service.GetByIdWithIncludes(articleId, userId);
+        return HandleResult(result);
     }
 
     [HttpPost("private/add")]
-    public async Task<IActionResult> AddPrivate()
+    public async Task<IActionResult> AddPrivate([FromBody] AddPrivateArticleDto dto)
     {
-        return Success();;
+        var userId = GetCurrentUserId();
+        
+        var result = await service.AddPrivate(dto, userId);
+        return HandleResult(result);
     }
 
     [HttpPost("public/add")]
-    public async Task<IActionResult> AddPublic()
+    public async Task<IActionResult> AddPublic([FromBody] AddPublicArticleDto dto)
     {
-        return Success();;
+        var userId = GetCurrentUserId();
+        
+        var result = await service.AddPublic(dto, userId);
+        return HandleResult(result);
     }
-
-    [HttpPut("private/update")]
-    public async Task<IActionResult> UpdatePrivate()
+    
+    [HttpPut("{articleId:guid}")]
+    public async Task<IActionResult> Update(Guid articleId, [FromBody] UpdateArticleDto dto)
     {
-        return Success();
-    }
-
-    [HttpPut("public/update")]
-    public async Task<IActionResult> UpdatePublic()
-    {
-        return Success();
+        var result = await service.Update(dto, articleId);
+        return HandleResult(result);
     }
 
     [HttpDelete("{articleId}/delete")]
     public async Task<IActionResult> Delete(Guid articleId)
     {
-        return Success();
+        var result = await service.Delete(articleId);
+        return HandleResult(result);
     }
 }
