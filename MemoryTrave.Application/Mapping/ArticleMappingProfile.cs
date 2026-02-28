2b2fc73b506f7cacc08dfbb7d4278de0373138dd
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:10:31 +0300

    up(ArticleMapping): add mappings #4

--- MemoryTrave.Application/Mapping/ArticleMappingProfile.cs
+++ MemoryTrave.Application/Mapping/ArticleMappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using MemoryTrave.Application.Dto.Requests.Article;
 using MemoryTrave.Application.Dto.Responses.Article;
+using MemoryTrave.Application.Dto.Responses.Article.GetArticle;
 using MemoryTrave.Domain.Enums;
 using MemoryTrave.Domain.Models;
 
@@ -22,5 +24,69 @@ public class ArticleMappingProfile : Profile
             .ForMember(dto => dto.Description,
                 opt => opt.MapFrom(src =>
                     src.Visibility == VisibilityEnum.Public ? src.Description : null));
+
+        CreateMap<Article, GetArticleBaseDto>()
+            .ForMember(dto => dto.AuthorName,
+                opt => opt.MapFrom(src =>
+                    src.Author.Username))
+            .ForMember(dto => dto.LocationName,
+                opt => opt.MapFrom(src =>
+                    src.Location.Name));
+        
+        CreateMap<Article, GetPrivateArticleDto>()
+            .IncludeBase<Article, GetArticleBaseDto>()
+            .ForMember(dto => dto.EncryptedKey,
+                opt => opt.Ignore());
+
+        CreateMap<Article, GetPublicArticleDto>()
+            .IncludeBase<Article, GetArticleBaseDto>();
+        
+        CreateMap<AddPrivateArticleDto, Article>()
+            .ForMember(mod => mod.Id, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.Visibility, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.CreatedAt, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.LastChange, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.EncryptedKeys, opt =>
+                opt.Ignore())
+            .ForMember(mod => mod.Description, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.PhotosUrls, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.AuthorId, opt =>
+                opt.Ignore());
+        
+        CreateMap<AddPublicArticleDto, Article>()
+            .ForMember(mod => mod.Id, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.Visibility, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.CreatedAt, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.LastChange, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.EncryptedPreviewData, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.EncryptedData, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.EncryptedKeys, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.AuthorId, opt =>
+                opt.Ignore());
+        
+        CreateMap<UpdateArticleDto, Article>()
+            .ForMember(mod => mod.Id, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.CreatedAt, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.EncryptedKeys, opt => 
+                opt.Ignore())
+            .ForMember(mod => mod.AuthorId, opt =>
+                opt.Ignore())
+            .ForMember(mod => mod.LocationId, opt =>
+                opt.Ignore());
     }
 }
