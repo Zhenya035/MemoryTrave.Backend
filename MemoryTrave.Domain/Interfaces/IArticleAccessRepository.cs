8908a35a1b470f9a0a287cfa57a14cc456b5424d
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:09:45 +0300

    up: articleAccess repository #4

--- MemoryTrave.Domain/Interfaces/IArticleAccessRepository.cs
+++ MemoryTrave.Domain/Interfaces/IArticleAccessRepository.cs
@@ -5,10 +5,10 @@ namespace MemoryTrave.Domain.Interfaces;
 public interface IArticleAccessRepository
 {
     public Task AddList(List<ArticleAccess> articleAccesses);
-    public Task Add(ArticleAccess articleAccess);
     
-    public Task DeleteList(List<Guid> ids);
-    public Task Delete(Guid id);
+    public Task Sync(Guid articleId, List<ArticleAccess> articleAccesses);
+    
+    public Task DeleteForArticle(Guid id);
     
     public Task<bool> Exists(Guid articleId, Guid userId);
 }
