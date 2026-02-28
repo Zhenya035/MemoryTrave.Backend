23ea66a9000d298c556c67d73b4804c74cc8e74d
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:06:40 +0300

    up(getArticleForProfileDto): add CreatedAt #4

--- MemoryTrave.Application/Dto/Responses/Article/GetArticleForProfileDto.cs
+++ MemoryTrave.Application/Dto/Responses/Article/GetArticleForProfileDto.cs
@@ -4,6 +4,7 @@ public class GetArticleForProfileDto
 {
     public Guid Id { get; set; }
     public string? LocationName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
     public DateTime LastChange { get; set; }
     public bool IsPrivate { get; set; }
     public string? EncryptedPreviewData  { get; set; }
