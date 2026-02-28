c287bca8b1ec2a81048b8a52df6f7d6e7a3448b0
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-01 00:06:06 +0300

    add: get artcle access dto #4

---
+++ MemoryTrave.Application/Dto/Responses/Article/Access/GetArticleAccessDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MemoryTrave.Application.Dto.Responses.Article.Access;
+
+public class GetArticleAccessDto
+{
+    public string EncryptedKey {get; set;} = string.Empty;
+    public Guid UserId { get; set; }
+    public Guid ArticleId { get; set; }
+}
