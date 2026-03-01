14822914915f8ae595173a5b98a80a0940677b0d
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 00:16:05 +0300

    up(friendRequestController): realization controller #4

--- MemoryTrave.Web/Controllers/Friends/FriendRequestController.cs
+++ MemoryTrave.Web/Controllers/Friends/FriendRequestController.cs
@@ -1,33 +1,49 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MemoryTrave.Application.Dto.Requests;
+using MemoryTrave.Application.Interfaces.Friend;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MemoryTrave.Web.Controllers.Friends;
 
 [Route("friends/requests")]
 [Authorize]
-public class FriendRequestController(IWebHostEnvironment env) : BaseController(env)
+public class FriendRequestController(
+    IFriendRequestService service,
+    IWebHostEnvironment env) : BaseController(env)
 {
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        throw new Exception();
+        var userId = GetCurrentUserId();
+        
+        var result = await service.GetAllByUserId(userId);
+        return HandleResult(result);
     }
 
-    [HttpPost("confirm")]
-    public async Task<IActionResult> Confirm()
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] IdDto dto)
     {
-        throw new Exception();
+        var userId = GetCurrentUserId();
+
+        var result = await service.Create(userId, dto.Id);
+        return HandleResult(result);
     }
 
-    [HttpDelete("cancel")]
-    public async Task<IActionResult> Cancel()
+    [HttpPost("{requestId:guid}/confirm")]
+    public async Task<IActionResult> Confirm(Guid requestId)
     {
-        throw new Exception();
+        var userId = GetCurrentUserId();
+        
+        var result = await service.Confirm(userId, requestId);
+        return HandleResult(result);
     }
 
-    [HttpDelete("delete")]
-    public async Task<IActionResult> Delete()
+    [HttpDelete("{requestId:guid}/cancel")]
+    public async Task<IActionResult> Cancel(Guid requestId)
     {
-        throw new Exception();
+        var userId = GetCurrentUserId();
+        
+        var result = await service.Cancel(userId, requestId);
+        return HandleResult(result);
     }
 }
