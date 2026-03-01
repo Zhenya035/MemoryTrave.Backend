78a7cab35c83275ada83fc32e11975f4bd11c1d2
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 00:12:58 +0300

    add: dtos for friend request #4

---
+++ MemoryTrave.Application/Dto/Requests/IdDto.cs
@@ -0,0 +1,6 @@
+﻿namespace MemoryTrave.Application.Dto.Requests;
+
+public class IdDto
+{
+    public Guid Id { get; set; }
+}
---
+++ MemoryTrave.Application/Dto/Responses/Friend/GetFriendRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MemoryTrave.Application.Dto.Responses.Friend;
+
+public class GetFriendRequestDto
+{
+    public Guid Id { get; set; }
+    public string FromUserName { get; set; }
+    public string ToUserName { get; set; }
+}
