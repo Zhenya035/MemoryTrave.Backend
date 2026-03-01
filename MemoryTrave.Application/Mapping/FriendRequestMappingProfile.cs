4aef43f29876d6ef5433e4446707e139fdaae0be
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 00:13:20 +0300

    add: mapping for friend request #4

---
+++ MemoryTrave.Application/Mapping/FriendRequestMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MemoryTrave.Application.Dto.Responses.Friend;
+using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Application.Mapping;
+
+public class FriendRequestMappingProfile : Profile
+{
+    public FriendRequestMappingProfile()
+    {
+        CreateMap<FriendRequest, GetFriendRequestDto>()
+            .ForMember(dto => dto.FromUserName,
+                opt => opt.MapFrom(
+                    src => src.FromUser.Username))
+            .ForMember(dto => dto.ToUserName,
+                opt => opt.MapFrom(
+                    src => src.ToUser.Username));
+    }
+}
