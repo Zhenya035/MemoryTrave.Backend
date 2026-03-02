0917336dee306346425e7d94542fb2292b847488
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 17:52:30 +0300

    add: getFriendship dto with mapping #4

---
+++ MemoryTrave.Application/Dto/Responses/Friend/GetFriendshipDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MemoryTrave.Application.Dto.Responses.Friend;
+
+public class GetFriendshipDto
+{
+    public Guid Id { get; set; }
+    public string FriendName { get; set; }
+}
---
+++ MemoryTrave.Application/Mapping/FriendshipMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MemoryTrave.Application.Dto.Responses.Friend;
+using MemoryTrave.Domain.Models;
+
+namespace MemoryTrave.Application.Mapping;
+
+public class FriendshipMappingProfile : Profile
+{
+    public FriendshipMappingProfile()
+    {
+        CreateMap<Friendship, GetFriendshipDto>()
+            .ForMember(dto => dto.FriendName,
+                opt => opt.MapFrom((src, dest, member, context) =>
+                {
+                    var currentUserId = (Guid)context.Items["UserId"];
+
+                    return src.UserId == currentUserId
+                        ? src.Friend.Username
+                        : src.User.Username;
+                }));
+    }
+}
