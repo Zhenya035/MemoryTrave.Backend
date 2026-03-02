fb337a9fb04b0a4f2e5df1387ca6c6e27a1389c5
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 22:46:10 +0300

    add: location dtos with validation and mapping #4

---
+++ MemoryTrave.Application/Dto/Responses/Location/GetAllLocationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MemoryTrave.Application.Dto.Responses.Location;
+
+public class GetAllLocationDto
+{
+    public Guid  Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Geohash { get; set; } =  string.Empty;
+}
---
+++ MemoryTrave.Application/Dto/Responses/Location/GetLocationDto.cs
@@ -0,0 +1,9 @@
+﻿using MemoryTrave.Application.Dto.Responses.Article.GetArticle;
+
+namespace MemoryTrave.Application.Dto.Responses.Location;
+
+public class GetLocationDto
+{
+    public string Name { get; set; } = string.Empty;
+    public List<GetArticleBaseDto> Articles { get; set; } = [];
+}
