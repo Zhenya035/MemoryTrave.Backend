d273030d4ba1420d95d762a449ef2af69210b7fc
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 22:50:20 +0300

    add: get preview aricle dto #4

---
+++ MemoryTrave.Application/Dto/Responses/Article/GetArticle/GetPreviewPrivateArticle.cs
@@ -0,0 +1,6 @@
+﻿namespace MemoryTrave.Application.Dto.Responses.Article.GetArticle;
+
+public class GetPreviewPrivateArticle : GetArticleBaseDto
+{
+    public string EncryptedPreviewData { get; set; } = string.Empty;
+}
