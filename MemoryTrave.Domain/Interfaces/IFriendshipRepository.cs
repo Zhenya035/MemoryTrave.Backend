3873ecad83e182e146beed5fcc3d3f3bf6ea4340
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-02 17:54:35 +0300

    up(friendshipRepository): add some methods #4

--- MemoryTrave.Domain/Interfaces/IFriendshipRepository.cs
+++ MemoryTrave.Domain/Interfaces/IFriendshipRepository.cs
@@ -4,8 +4,12 @@ namespace MemoryTrave.Domain.Interfaces;
 
 public interface IFriendshipRepository
 {
-    public Task<List<Guid>> GetAllFriends(Guid userId);
+    public Task<List<Friendship>> GetAllFriends(Guid userId);
+    public Task<Friendship?> GetById(Guid friendshipId);
     
     public Task Add(Friendship friendship);
+    
+    public Task Delete(Guid friendshipId);
+    
     public Task<bool> ExistByUsers(Guid userId, Guid anotherUserId);
 }
