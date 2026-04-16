5edfee6f448d1de93845e6fe5aa5190ac3093274
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-04-16 20:40:16 +0300

    add: dtos #19

---
+++ MemoryTrave.Application/Dto/Requests/Article/Access/AddAccessForFriendDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MemoryTrave.Application.Dto.Requests.Article.Access;
+
+public class AddAccessForFriendDto
+{
+    public string EncryptedKey {get; set;} = string.Empty;
+    public Guid ArticleId { get; set; }
+}
