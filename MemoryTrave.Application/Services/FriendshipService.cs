45cd0d8705ee7c8e8c0dc14023ec0ba1009c0ede
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-05-21 01:03:53 +0300

    up(friendship): add to get all

--- MemoryTrave.Application/Services/FriendshipService.cs
+++ MemoryTrave.Application/Services/FriendshipService.cs
@@ -10,6 +10,8 @@ namespace MemoryTrave.Application.Services;
 public class FriendshipService(
     IFriendshipRepository repository,
     IUserRepository userRepository,
+    IArticleRepository articleRepository,
+    IArticleAccessRepository articleAccessRepository,
     IMapper mapper) : IFriendshipService
 {
     public async Task<Result<List<GetOtherDto>>> GetAll(Guid userId)
@@ -22,6 +24,26 @@ public class FriendshipService(
         
         var result = mapper.Map<List<GetOtherDto>>(friendship,
             opt => opt.Items["UserId"] = userId);
+        
+        return Result<List<GetOtherDto>>.Success(result);
+    }
+
+    public async Task<Result<List<GetOtherDto>>> GetAllWithFriendId(Guid userId)
+    {
+        var isExist = await userRepository.ExistsById(userId);
+        if (!isExist)
+            return Result<List<GetOtherDto>>.Failure("User not found", ErrorCode.NotFound);
+            
+        var friendship = await repository.GetAllFriends(userId);
+        
+        var result = mapper.Map<List<GetOtherDto>>(friendship,
+            opt => opt.Items["UserId"] = userId);
+        
+        foreach (var friend in result)
+        {
+            var first = friendship.First(u => u.Id == friend.Id);
+            friend.Id = first.UserId == userId ? first.FriendId : first.UserId;
+        }
         
         return Result<List<GetOtherDto>>.Success(result);
     }
@@ -50,6 +72,13 @@ public class FriendshipService(
         if (friendship.UserId != userId && friendship.FriendId != userId)
             return Result.Failure("User is not in the friendsip", ErrorCode.AccessDenied);
 
+        var userArticles = await articleRepository.GetArticlesIdsFromUser(userId);
+        var friendId = friendship.UserId == userId ? friendship.FriendId : friendship.UserId;
+        var friendArticles = await articleRepository.GetArticlesIdsFromUser(friendId);
+
+        await articleAccessRepository.DeleteForUser(userArticles, friendId);
+        await articleAccessRepository.DeleteForUser(friendArticles, userId);
+        
         await repository.Delete(friendshipId);
         return Result.Success();
     }
