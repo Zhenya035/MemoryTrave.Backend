6f0a3b29623b1e7026c84ac4325e45736be3964f
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-05-21 01:04:35 +0300

    up(friendRequest): add to get ids

--- MemoryTrave.Domain/Interfaces/IFriendRequestRepository.cs
+++ MemoryTrave.Domain/Interfaces/IFriendRequestRepository.cs
@@ -6,6 +6,7 @@ public interface IFriendRequestRepository
 {
     public Task<List<FriendRequest>> GetAllToUserId(Guid userId);
     public Task<List<FriendRequest>> GetAllFromUserId(Guid userId);
+    public Task<List<Guid>> GetAllRequestIds(Guid userId);
     public Task<FriendRequest?> GetById(Guid requestId);
     
     public Task Create(FriendRequest request);
